d7a95c606d1c77c6bb8073959891c26c5e93e506
codefish <2358514288@qq.com>
2025-03-09 22:10:08 +0800

    move  tools path

--- packages/host/rsbuild.config.ts
+++ packages/host/rsbuild.config.ts
@@ -3,22 +3,19 @@ import { pluginVue } from '@rsbuild/plugin-vue';
 import { defineConfig } from '@rsbuild/core';
 import { merge } from 'webpack-merge';
 // import { mergeConfig } from '@rsbuild/core';
-import { loadNxEnv } from '../../tools';
+import { loadNxEnv } from '../../tools/src';
 import path from 'path';
 
 const env = loadNxEnv(__dirname);
-console.log('Resolved Env:', env);
-if (!env.success) {
-  console.error('Missing Env Files:', [
-    '.env',
-    '.env.development',
-    '.env.local'
-  ].map(f => path.join(__dirname, f)));
-}
+console.log('环境变量解析结果:', {
+  success: env.success,
+  data: env.success ? env.data : 'N/A',
+  error: env.success ? 'N/A' : env.error.format()
+});
 
 const baseConfig = require('../../rsbuild.config.ts');
 
-const port = Number(env.data.HOST_PORT);
+const port = Number(3000);
 const validPort = Number.isInteger(port) && port > 0 ? port : 3000;
 
 export default defineConfig(merge(baseConfig, {
@@ -44,6 +41,9 @@ export default defineConfig(merge(baseConfig, {
   },
   server: {
     port: validPort,
+    logger: {
+      level: 'verbose'
+    }
   },
   output: {
     target: 'web',
